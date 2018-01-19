96ce5d7645d5d16ca2c92a626cbd85e1fa82703f
lizhsen <lizhsen@163.com>
2018-01-19 17:57:12 +0800

    add more functions
    Signed-off-by: lizhsen <lizhsen@163.com>

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,7 +1,10 @@
+# encoding: utf-8
+
 from settings import Settings
 import pygame
 from ship import Ship
 import game_functions as gf
+from pygame.sprite import Group
 
 
 def run_game():
@@ -9,12 +12,16 @@ def run_game():
     ai_setting = Settings()
     screen = pygame.display.set_mode((ai_setting.screen_width, ai_setting.screen_height))
     pygame.display.set_caption("Alien Invasion")
+    # 创建一个用于存储子弹的编组
     ship = Ship(ai_setting, screen)
+    # 创建一个子弹编组
+    bullets = Group()
 
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_setting, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_setting, screen, ship)
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_setting, screen, ship, bullets)
 
 
 run_game()
